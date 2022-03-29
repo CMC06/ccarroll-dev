45a697fd5aa420b5fd9b669a2a806287d449e398
CMC06 <crzychelc@gmail.com>
2022-03-29 11:36:28 -0400

    removing eye CSS project and updating order of displayed project cards

--- src/Projects.js
+++ src/Projects.js
@@ -1,45 +1,6 @@
 const projects =
   [
     {
-      navname: "PacMan Browser Game",
-      hash: "#pacHash",
-      id :"PacManCard",
-      imgSrc: "./pacmanSnip.png",
-      imgAlt: "Image from PacMan project",
-      title: "PacMan Inspired Browser Game",
-      description: "A simple browser-based game inspired by the PacMan arcade game.",
-      demourl: "https://cmc06.github.io/PacManProject/PacMan.html",
-      repourl: "https://github.com/CMC06/PacManProject",
-      languages: "JavaScript, HTML, CSS"
-    },  
-
-    {
-      navname: "Monster Eyes",
-      hash: "#MonsterEyesCard",
-      id :"MonsterEyesCard",
-      imgSrc: "./oneeye.png",
-      imgAlt: "Image from Monster Eye Animation project",
-      title: "Monster Eye Animation",
-      description: "Animation exercise involving eye tracking following mouse movement.",
-      demourl: "https://cmc06.github.io/MonsterEyesAnimation/eyes.html",
-      repourl: "https://github.com/CMC06/MonsterEyesAnimation",
-      languages: "JavaScript, HTML, CSS"
-    },
-
-    {
-      navname: "Bus Tracker",
-      hash: "#BusTrackerCard",
-      id :"BusTrackerCard",
-      imgSrc: "./busTrackerSnip.jpg",
-      imgAlt: "Image from Boston Bus Tracker Project",
-      title: "Boston Realtime Bus Tracker",
-      description: "Animated map tracking Route 1 Boston buses in real-time using MBTA's API.",
-      demourl: "https://cmc06.github.io/BostonRealTimeBusTracker/real-time_bus_tracker.html",
-      repourl: "https://github.com/CMC06/BostonRealTimeBusTracker",
-      languages: "JavaScript, HTML, CSS"
-    },
-
-    {
       navname: "Task Tracker",
       hash: "#TaskTrackerCard",
       id :"TaskTrackerCard",
@@ -63,7 +24,33 @@ const projects =
       demourl: "https://cmc06.github.io/tic-tac-toe/",
       repourl: "https://github.com/CMC06/tic-tac-toe",
       languages: "React, JavaScript, HTML, CSS"
+    }, 
+
+    {
+      navname: "Bus Tracker",
+      hash: "#BusTrackerCard",
+      id :"BusTrackerCard",
+      imgSrc: "./busTrackerSnip.jpg",
+      imgAlt: "Image from Boston Bus Tracker Project",
+      title: "Boston Realtime Bus Tracker",
+      description: "Animated map tracking Route 1 Boston buses in real-time using MBTA's API.",
+      demourl: "https://cmc06.github.io/BostonRealTimeBusTracker/real-time_bus_tracker.html",
+      repourl: "https://github.com/CMC06/BostonRealTimeBusTracker",
+      languages: "JavaScript, HTML, CSS"
     },
+
+    {
+      navname: "PacMan Browser Game",
+      hash: "#pacHash",
+      id :"PacManCard",
+      imgSrc: "./pacmanSnip.png",
+      imgAlt: "Image from PacMan project",
+      title: "PacMan Inspired Browser Game",
+      description: "A simple browser-based game inspired by the PacMan arcade game.",
+      demourl: "https://cmc06.github.io/PacManProject/PacMan.html",
+      repourl: "https://github.com/CMC06/PacManProject",
+      languages: "JavaScript, HTML, CSS"
+    }, 
   ]
 
 export default projects
