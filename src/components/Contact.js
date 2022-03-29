f435f9d52561e49faa8479dcf731bbccbebc299d
CMC06 <crzychelc@gmail.com>
2022-03-29 11:31:40 -0400

    removing email address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,6 @@ const Contact = () => {
       <ul>
         <li className="contactList"><div><img src={images("./GitHub-Mark.png").default} alt="Git Hub Mark Icon" height="30px" /><a href="https://github.com/CMC06" className="plainLink">  GitHub Profile</a></div></li>
         <li className="contactList"><div><img src={images("./LI-In-Bug.png").default} alt="LinkedIn small icon" height="30px" /><a href="https://www.linkedin.com/in/chelsea-carroll-205477102/" className="plainLink">  LinkedIn Profile</a></div></li>
-        <li className="contactList"><div><img src={images("./mail-icon.png").default} alt="email small icon" height="30px" /><a href="mailto:chelsea.m.carroll.06@gmail.com" className="plainLink">  chelsea.m.carroll.06@gmail.com</a></div></li>
       </ul>
     </Container>
   );
